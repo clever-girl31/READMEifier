3ff1233075925c23bcb706247666c2c6830270c6
clever-girl31 <johnmseybold@gmail.com>
2023-05-01 12:18:42 -0500

    got it to make a file

--- index.js
+++ index.js
@@ -1,8 +1,38 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 
-const CreateReadMe = ({placeholder1, placeholder2, placeholder3, placeholder4}) =>
-`README script in here, template literal frmt`;
+const createReadMe = ({title, description, installation, usage, contribution, test, license, username, email }) =>
+`# ${title}
+
+## Description:
+${description}
+
+## Table of Contents:
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation:
+${installation}
+
+## Usage:
+${usage}
+
+## License:
+${license}
+
+## Contributing:
+${contribution}
+
+## Tests:
+${test}
+
+## Questions:
+Questions about this application or other work by this dev can be directed to: ${email}
+Additional work from this dev can be found at [their GitHub profile](github.com/${username})
+`;
 
 
 inquirer
@@ -39,16 +69,16 @@ inquirer
     type: 'input',
     name: 'username',
     message: 'Enter GitHub username here:'
+  },{
+    type: 'input',
+    name: 'email',
+    message: 'Enter your email address here:',
   }
   ])
   .then((answers) => {
-    
-  })
-  .catch((error) => {
-    if (error.isTtyError) {
-      // Prompt couldn't be rendered in the current environment
-    } else {
-      // Something else went wrong
-    }
+    const indexContent = createReadMe(answers);
+
+    fs.writeFile('./output/READMEEM.md', indexContent, (err) =>
+    err ? console.log(err) : console.log('ReadME successfully generated'))
   });
 
---
+++ output/READMEEM.md
@@ -0,0 +1,30 @@
+# asdf
+
+## Description:
+asdf
+
+## Table of Contents:
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation:
+asdf
+
+## Usage:
+asdf
+
+## License:
+1
+
+## Contributing:
+asdf
+
+## Tests:
+asdf
+
+## Questions:
+Questions about this application or other work by this dev can be directed to: asdf
+Additional work from this dev can be found at [their GitHub profile](github.com/asdf)
